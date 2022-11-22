02b32c952aa21cec5c623f35357a77ac18ef842b
LoganLS <logan_56@hotmail.fr>
2022-11-22 16:34:43 +0100

    init hack

--- src/Components/Hack/index.js
+++ src/Components/Hack/index.js
@@ -2,10 +2,22 @@ import { useState } from "react";
 import QRCode from "react-qr-code";
 
 export default function Hack({ route }) {
-    console.log(route);
+    const supported = 'contacts' in navigator;
+
+    async function hack() {
+        console.log('hack');
+        const supportedProperties = await navigator.contacts.getProperties();
+        if (supportedProperties.includes('tel')) {
+            const contacts = await navigator.contacts.select();
+            console.log(contacts);
+        } else {
+            alert('Pas de contact à hacker');
+        }
+      }
 
     return (
         <div>
+            <button className="button" onClick={() => hack()}>Hack</button><br/>
             <QRCode value={process.env.REACT_APP_QR_CODE} />
         </div>
     )
